b947d0d43373e6efa8b5b0ea7dd19e69427bf46c
ATTADASAIPRAHARSHA <saipraharshaattada@gmail.com>
2024-05-27 23:25:15 +0530

    maybe011

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import Header from './components/Header.jsx'
+import App from './App.jsx';
+import Header from './components/Header.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Header/>
-    <App />
+      <Header />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
